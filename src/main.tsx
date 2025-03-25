df5effc72a9405fc25372025467a5a7ac4105c39
j0ebin <joeytowbin@gmail.com>
2025-03-25 16:04:31 -0700

    feat: add login page and update main with auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App.tsx'
 import './index.css'
+import { AuthProvider } from './contexts/AuthContext'
 
 // Create a client
 const queryClient = new QueryClient()
@@ -11,7 +11,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </QueryClientProvider>
   </BrowserRouter>
 );
